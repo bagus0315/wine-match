764b90f60294b06346520ac96cd1fc490d2693a7
dimas0315 <asamieto965@gmail.com>
2025-03-31 07:31:39 +0700

    fix : country property

--- pages/countries/[slug].tsx
+++ pages/countries/[slug].tsx
@@ -61,3 +61,33 @@ export async function getStaticProps({ params }) {
     },
   }
 }
+
+
+
+// export default function Countries({ country }) {
+//   return (
+//     <>
+//       <Seo title={`${country.name} - Wine Match`} image={country.flag} />
+//       <div className="my-3 flex items-center py-2">
+//         <Image
+//           width={120}
+//           height={72}
+//           src={country.flag}
+//           alt={country.name}
+//           className="mx-3 rounded-md opacity-70"
+//         />
+//         <div className="w-full text-2xl font-bold text-slate-800 dark:text-slate-300">
+//           {country.name}
+//           <div className="text-medium my-1 flex items-center font-medium text-slate-600 dark:text-slate-400">
+//             <FlagIcon className="mr-1 h-5 w-5" />
+//             {country.count_regions} regions and {country.count_wines} wines
+//           </div>
+//         </div>
+//       </div>
+//       <PageInstantSearch
+//         attribute="region"
+//         filters={`country: "${country.name}"`}
+//       />
+//     </>
+//   )
+// }
